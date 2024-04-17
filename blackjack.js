126feaf9ba10a0b8da01a3cebea9cb3655bfd25d
Lynette <90663407+LynetteLiu2333@users.noreply.github.com>
2024-04-17 16:55:53 -0600

    Implement improved version of the buildDeck and shuffleDeck functions

--- blackjack.js
+++ blackjack.js
@@ -13,22 +13,20 @@ window.onload = function() {
 }
 
 function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"];
+    const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const cardTypes = ["C", "D", "H", "S"];
     deck = [];
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + types[i]);
+    for (const type of cardTypes) {
+        for (const value of cardValues) {
+            deck.push(`${value}-${type}`);
         }
     }
 }
 
 function shuffleDeck() {
-    for (let i = 0; i < deck.length; i++) {
-        let j = Math.floor(Math.random() * deck.length);
-        let temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
     }
 }
 
